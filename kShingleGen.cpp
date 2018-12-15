87d12fe6c98e9e0a753f15533e0bb398c86671b2
Oscar Garcia <narcyz2048@gmail.com>
2018-12-15 20:23:02 +0100

    Canvis menors i afegits stats

--- kShingleGen.cpp
+++ kShingleGen.cpp
@@ -11,61 +11,51 @@ bool kShingling(string path,int k, set<string>& shingles){
 		exit(1);
 	}
 
+
 	string total = "";
+	int words = 0;
+	int chars = 0;
 	while (inFile >> s){
+		words++;
 		for (char c : s)
 		{
+			chars++;
 			if (c >= 'A' and c<= 'Z')
 			{
 				c = tolower(c);
 				total += c;
-			} else if (c>='a' and c<='z')
+			} else if ((c>='a' and c<='z') or (c>='0' and c<='9'))
 			{
 				total += c;
 			}
 		}
+		total += " ";
 		
 	}
-	//cout << total << endl;
 
-	//set<string> shingles;
+	set<string>::iterator it = shingles.begin();
+	int schars = 0;
+	int shin = 0;
 	if (total.length()>=k)
 	{
 		cout << "Valid document: creating " << k << "-shingles" << endl;
 		for (int i = 0; i < total.length(); i = i+k)
 		{
+			schars++;
 			if (i+k <= total.length())
 			{
-				shingles.insert(total.substr(i,k));
+				shin++;
+				shingles.insert(it,total.substr(i,k));
+				it++;
 			}
 		}
+		cout << "Generació finalitzada. Stats:" << endl;
+		cout << "Paraules del document:" << words << endl;
+		cout << "Caràcters analitzats del document:" << chars << endl;
+		cout << "Caràcters considerats per a " << k <<"-shingles:" << schars << endl;
+		cout << "Número de " << k << "-shingles:" << shin << endl;
 
 		return true;
 
 	} else return false;
 }
-
-
-/*int main(){
-	set<string> C1, C2;
-	string doc1 = "doc1.txt";
-	string doc2 = "doc2.txt";
-	if (kShingling(doc1,2,C1))
-	{
-		for (string s : C1)
-		{
-			cout << s << endl;
-		}
-
-		if (kShingling(doc2,2,C2))
-		{
-			for (string s : C2)
-			{
-				cout << s << endl;
-			}
-		}
-	}
-
-
-
-}*/
